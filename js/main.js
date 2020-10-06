032d98f50de2b0a141a21f118cd8fb3a5bc51d55
Neer Bappoo <neerbappoo@gmail.com>
2020-10-07 03:22:16 +0400

    update

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var slider = tns({
+
+    speed: 2000,
+    container: ".my-slider",
+    controls: true,
+    controlsContainer: ".js-controls",
+    mode: "carousel",
+    /*controls: false,*/
+    controlsText: false,
+    nav: false,
+    autoplayButtonOutput: false,
+    autoHeight: false,
+    mouseDrag: true,
+    responsive: {
+        360: {
+            items: 2
+        },
+        600: {
+            items: 4
+        },
+        900: {
+            items: 6
+        },
+    },
+
+});
+var slider = tns({
+
+    speed: 2000,
+    container: ".my-slider",
+    controls: true,
+    controlsContainer: ".js-controls",
+    mode: "carousel",
+    /*controls: false,*/
+    controlsText: false,
+    nav: false,
+    autoplayButtonOutput: false,
+    autoHeight: false,
+    mouseDrag: true,
+    responsive: {
+        360: {
+            items: 2
+        },
+        600: {
+            items: 4
+        },
+        900: {
+            items: 6
+        },
+    },
+
+});
