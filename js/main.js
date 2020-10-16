ce76d673f89bfc9c7d640da43a8626c5b486e365
Neer Bappoo <neerbappoo@gmail.com>
2020-10-16 20:33:51 +0400

    update

--- js/main.js
+++ js/main.js
@@ -1,52 +1,51 @@
-var slider = tns({
+$ = jQuery.noConflict();
 
-    speed: 2000,
-    container: ".my-slider",
-    controls: true,
-    controlsContainer: ".js-controls",
-    mode: "carousel",
-    /*controls: false,*/
-    controlsText: false,
-    nav: false,
-    autoplayButtonOutput: false,
-    autoHeight: false,
-    mouseDrag: true,
-    responsive: {
-        360: {
-            items: 2
-        },
-        600: {
-            items: 4
-        },
-        900: {
-            items: 6
-        },
-    },
+jQuery(function($) {
 
-});
-var slider = tns({
+    [].forEach.call(document.querySelectorAll('.my-slider'), function (el) {
+        tns({
 
-    speed: 2000,
-    container: ".my-slider",
-    controls: true,
-    controlsContainer: ".js-controls",
-    mode: "carousel",
-    /*controls: false,*/
-    controlsText: false,
-    nav: false,
-    autoplayButtonOutput: false,
-    autoHeight: false,
-    mouseDrag: true,
-    responsive: {
-        360: {
-            items: 2
-        },
-        600: {
-            items: 4
-        },
-        900: {
-            items: 6
-        },
-    },
+            speed: 2000,
+            container: el,
+            controls: true,
+            controlsContainer: ".js-controls",
+            mode: "carousel",
+            /*controls: false,*/
+            controlsText: false,
+            nav: false,
+            autoplayButtonOutput: false,
+            autoHeight: false,
+            mouseDrag: true,
+            responsive: {
+                360: {
+                    items: 2
+                },
+                600: {
+                    items: 4
+                },
+                900: {
+                    items: 6
+                },
+            },
+
+        });
+    });
+
+//scroll to appear
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 1000)
+            $('.scroll-top-arrow').fadeIn('slow');
+        else
+            $('.scroll-top-arrow').fadeOut('slow');
+    });
+
+
+//Click event to scroll to top
+    $(document).on('click', '.scroll-top-arrow', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 
 });
